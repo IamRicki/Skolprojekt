c905deede7b8a29a601a32f3b533a6612215be26
IamRicki <101406372+IamRicki@users.noreply.github.com>
2022-05-11 13:33:06 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+window.addEventListener("load", function(){
+    /* ----------------- Animation när man skrollar -------------------- */
+    AOS.init();
+});
+
+/* ----------------- Aktivera menyn -------------------- */
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav(){
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
+}
+
+/* Stäng av när man trycker på nav föremål */
+document.addEventListener("click", function(e){
+if(e.target.closest(".nav-item")){
+    toggleNav();
+}
+});
+
+/*--------------------- sticky header (skroll) --------------------*/
+window.addEventListener("scroll", function(){
+    if(this.pageYOffset > 60){
+        document.querySelector(".header").classList.add("sticky");
+    }
+    else{
+        document.querySelector(".header").classList.remove("sticky");
+    }
+});
+
+/*--------------------- Meny alternativ för maten --------------------*/
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click", function(e){
+    if(e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")){
+        const target = e.target.getAttribute("data-target");
+        menuTabs.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const menuSection = document.querySelector(".menu-section");
+        menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+        menuSection.querySelector(target).classList.add("active");
+    }
+});
